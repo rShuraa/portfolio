6532aeb424fc25682b7e4c1f50e555417edcbd2a
Jonay Galindo <JonayGalindoCabrera@gmail.com>
2023-10-29 02:19:57 +0100

    Creación de cursor y sección de Tecnologías
    Se ha creado un puntero circular que sigue al cursor por la pantalla, este tiene animaciones al pulsar la pantalla, al salir y entrar en la página.
    
    También se ha creado la sección de tecnologías sin terminar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import Cursor from './components/cursorFollower';
 import Navbar from './components/Navbar';
 import Home from './components/home';
+import Tecnologias from './components/tecnologias';
+
 
 function App() {
+
   return (
     <div className="App">
-     <Navbar/>
-     <Home/>
+      <Cursor/>
+      <Navbar/>
+      <Home/>
+      <Tecnologias/>
+      
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/tecnologias.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./tecnologias.css"
+
+function Tecnologias() {
+  return (
+    <>
+      <section id="tecnologias">
+        <h2>Mis tecnologías</h2>
+        <div className="tecnologias">
+
+
+
+
+          <div>
+            <div className="techSubtitles">
+              <img/>
+              <h3>
+                <span>Software</span>Development
+              </h3>
+            </div>
+
+
+            <div className="techDesc">
+              <p>
+                Experienced in both functional and OOP: Dart, Python, Java,
+                JavaScript, TypeScript.
+              </p>
+            </div>
+          </div>
+
+
+
+
+
+          <div>
+            <div className="techSubtitles">
+              <img/>
+              <h3>
+                <span>Software</span>Development
+              </h3>
+            </div>
+
+
+            <div className="techDesc">
+              <p>
+                Experienced in both functional and OOP: Dart, Python, Java,
+                JavaScript, TypeScript.
+              </p>
+            </div>
+          </div>
+
+
+
+
+
+          <div>
+            <div className="techSubtitles">
+              <i className="fa-brands fa-react"></i>
+              <h3>
+                <span>Software</span><span>Development</span>
+              </h3>
+            </div>
+
+
+            <div className="techDesc">
+              <p>
+                Experienced in both functional and OOP: Dart, Python, Java,
+                JavaScript, TypeScript.
+              </p>
+            </div>
+          </div>
+
+
+
+
+
+
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Tecnologias;
